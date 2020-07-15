0ff48bf9557987a02dff054c50388da4d8555b9f
Maly <migsking@naver.com>
2020-07-15 18:03:30 +0900

    sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+sudo killall apt apt-get;
+sudo rm /var/lib/apt/lists/lock; 
+sudo rm /var/cache/apt/archives/lock;
+sudo rm /var/lib/dpkg/lock*;
+sudo dpkg --configure -a;
+sudo apt update;
+sudo apt install docker.io
