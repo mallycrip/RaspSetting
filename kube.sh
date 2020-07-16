601fdfb50025a21ddb31148b2655fa6267c39835
Maly <migsking@naver.com>
2020-07-16 13:30:58 +0900

    add #/bin/bash

--- kube.sh
+++ kube.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 sudo apt install apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
